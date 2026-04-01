296da5abcac8ba5ef7430940070d7073cf0e92a7
Christina Bradley <cmbrad@umich.edu>
2026-04-01 08:32:05 -0400

    Update content, fix headshot, fix CV base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://christinambradley.com',
-  // If deploying to GitHub Pages WITHOUT a custom domain, uncomment the line below
-  // and replace "your-repo-name" with your actual GitHub repository name:
-  // base: '/your-repo-name',
+  site: 'https://cmbrad98.github.io',
+  base: '/personal-website',
 });
